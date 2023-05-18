f54cdbaa7550096786824d21d147e3e3401a1b1d
Jonas Haslbeck <jonashaslbeck@protonmail.com>
2023-05-18 11:30:02 +0000

    version 0.1.1

--- inst/doc/Vignette_mlVAR_GC.R
+++ inst/doc/Vignette_mlVAR_GC.R
@@ -10,23 +10,21 @@ knitr::opts_chunk$set(
 library(mnet)
 
 ## -----------------------------------------------------------------------------
-data("mlVARGD_sim")
+data("ExampleData")
 
-dim(data1)
-head(data1)
+dim(ExampleData)
+head(ExampleData)
 
-dim(data2)
-head(data2)
 
 ## -----------------------------------------------------------------------------
 set.seed(1) # reproducibility
 
 timer <- proc.time()[3]
 
-output <- mlVAR_GC(data1 = data1, 
-                   data2 = data2, 
+output <- mlVAR_GC(data = ExampleData, 
                    vars = c("V1", "V2", "V3"), 
-                   idvar = "id", 
+                   idvar = "id",
+                   groups = "group",
                    nCores = 1, # choose cores available on your machine
                    nP = 5, # Should be much more in practice, see preprint!
                    pbar = FALSE)
@@ -34,15 +32,14 @@ output <- mlVAR_GC(data1 = data1,
 proc.time()[3] - timer
 
 ## -----------------------------------------------------------------------------
-output$TrueDiffs
+output$EmpDiffs
 
 ## -----------------------------------------------------------------------------
 output$Pval
 
 ## ---- fig.align="center", fig.width=7, fig.height=5---------------------------
-
-hist(output$SampDist$Phi_mean[1,2,], 
+hist(output$SampDist$Lagged_fixed[1,2,], 
      xlim=c(-.5, .5), main="", xlab="Parameter Value")
-abline(v=output$TrueDiffs$Phi_mean[1,2,], col="orange")
+abline(v=output$EmpDiffs$Lagged_fixed[1,2,], col="orange")
 legend("topright", legend="Test-statistic", text.col="orange", bty="n")
 
--- tests/testthat/test-mlVAR_GC.R
+++ tests/testthat/test-mlVAR_GC.R
@@ -5,15 +5,15 @@ test_that("mlVAR Perm Test check: P-values", {
   set.seed(1)
 
   # Call Permutation test on exampe data
-  out <- mlVAR_GC(data1 = data1,
-                  data2 = data2,
+  out <- mlVAR_GC(data = ExampleData,
                   vars = c("V1", "V2", "V3"),
                   idvar = "id",
+                  groups = "group",
                   nCores = 1,
                   nP = 5)
 
   # Check whether group difference is the same
-  expect_equal(out$Pval$Phi_mean[1,2],  0)
+  expect_equal(out$Pval$Lagged_fixed[1,2],  0)
 
 })
 
